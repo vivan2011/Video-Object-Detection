8d4fecf1d647c15dd54c8112442b60ab75462f00
vivan2011 <81799840+vivan2011@users.noreply.github.com>
2023-08-04 20:26:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+video="";
+statuss="";
+
+function preload(){
+    video=createVideo('video.mp4');
+    video.hide();
+}
+
+function setup(){
+    canvas=createCanvas(480,380);
+    canvas.center();
+}
+
+function draw(){
+    image(video,0,0,480,380);
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").InnerHTML="Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log( " The Model  has Loaded!Yahh!")
+    statuss=true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+
+}
